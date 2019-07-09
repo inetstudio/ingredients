f74441fb3b4ef49fb6fafdeecba7bbb72f766ddb
Konstantin Bukin <bukin@inetstudio.ru>
2019-07-09 15:31:21 +0300

    Update comments

--- entities/ingredients/src/Models/IngredientModel.php
+++ entities/ingredients/src/Models/IngredientModel.php
@@ -12,13 +12,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use InetStudio\Rating\Models\Traits\Rateable;
 use InetStudio\Uploads\Models\Traits\HasImages;
 use InetStudio\Widgets\Models\Traits\HasWidgets;
-use InetStudio\Comments\Models\Traits\HasComments;
 use InetStudio\Favorites\Models\Traits\Favoritable;
 use InetStudio\MetaPackage\Meta\Models\Traits\HasMeta;
 use InetStudio\TagsPackage\Tags\Models\Traits\HasTags;
 use InetStudio\Classifiers\Models\Traits\HasClassifiers;
 use InetStudio\AdminPanel\Base\Models\Traits\SluggableTrait;
 use InetStudio\StatusesPackage\Statuses\Models\Traits\Status;
+use InetStudio\CommentsPackage\Comments\Models\Traits\HasComments;
 use InetStudio\AdminPanel\Base\Models\Traits\Scopes\BuildQueryScopeTrait;
 use InetStudio\IngredientsPackage\Ingredients\Contracts\Models\IngredientModelContract;
 
